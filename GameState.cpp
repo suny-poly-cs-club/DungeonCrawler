3a41f50b20f4693d431e47b26044fc655017102e
John Harlan <Harlanj2>
2019-02-14 21:15:56 -0500

    Implementation of GameState and addition to header

--- GameState.cpp
+++ GameState.cpp
@@ -1,12 +1,28 @@
-#include "GameState.h"
+#include "GameState.hpp"
+#include <chrono>
 
-
-
-GameState::GameState()
-{
+GameState::GameState(){
 }
 
+GameState::~GameState(){
+}
 
-GameState::~GameState()
-{
+void GameState::updateState(){
+  this->_lastFrame = std::chrono::steady_clock::now();
+}
+
+long GameState::getTimeMilliseconds(){
+  return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - this->_lastFrame).count();
+}
+
+long GameState::getTimeSeconds(){
+  return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - this->_lastFrame).count();
+}
+
+void GameState::setPause(bool pState){
+  this->paused = pState;
+}
+
+bool GameState::isPaused(){
+  return this->paused;
 }
--- GameState.hpp
+++ GameState.hpp
@@ -1,13 +1,25 @@
 #pragma once
+#include <chrono>
+
 class GameState
 {
 public:
 	GameState();
 	~GameState();
 
-	double getTimeMilliseconds();
-	double getTimeSeconds();
+	//Gets the time since updateState() was last called
+	long getTimeMilliseconds();
+	long getTimeSeconds();
+
+	//Used to set the reference point for getTimeMilliseconds and getTimeSeconds
+	void updateState();
 
 	bool isPaused();
-};
+	void setPause(bool pState);
 
+private:
+	//The last time that updateState was called
+	std::chrono::steady_clock::time_point _lastFrame;
+
+	bool paused;
+};
